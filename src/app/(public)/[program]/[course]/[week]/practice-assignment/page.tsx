5c48ec74f79384e3c858bcfa77f68e124c2a3491
mishratejash01 <mtejash07@gmail.com>
2026-09-23 21:45:55 +0530

    style(app): format page.tsx with Prettier

--- src/app/(public)/[program]/[course]/[week]/practice-assignment/page.tsx
+++ src/app/(public)/[program]/[course]/[week]/practice-assignment/page.tsx
@@ -1,15 +1,23 @@
 import type { Metadata } from 'next'
 
-import { AssignmentRoute, assignmentMetadata, assignmentStaticParams } from '@/components/assignment/assignment-route'
+import {
+  AssignmentRoute,
+  assignmentMetadata,
+  assignmentStaticParams,
+} from '@/components/assignment/assignment-route'
 
 export async function generateStaticParams() {
   return assignmentStaticParams('practice', false)
 }
 
-export async function generateMetadata({ params }: PageProps<'/[program]/[course]/[week]/practice-assignment'>): Promise<Metadata> {
+export async function generateMetadata({
+  params,
+}: PageProps<'/[program]/[course]/[week]/practice-assignment'>): Promise<Metadata> {
   return assignmentMetadata(await params, 'practice')
 }
 
-export default async function Page({ params }: PageProps<'/[program]/[course]/[week]/practice-assignment'>) {
+export default async function Page({
+  params,
+}: PageProps<'/[program]/[course]/[week]/practice-assignment'>) {
   return <AssignmentRoute params={await params} kind="practice" />
 }
