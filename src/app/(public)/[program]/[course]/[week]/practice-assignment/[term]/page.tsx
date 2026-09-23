4a73b072cdfd8c1d81663ea5e0ae2309c9d4a2fc
mishratejash01 <mtejash07@gmail.com>
2026-09-23 21:45:55 +0530

    style(app): format page.tsx with Prettier

--- src/app/(public)/[program]/[course]/[week]/practice-assignment/[term]/page.tsx
+++ src/app/(public)/[program]/[course]/[week]/practice-assignment/[term]/page.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from 'next'
 
-import { AssignmentRoute, assignmentMetadata, assignmentStaticParams } from '@/components/assignment/assignment-route'
+import {
+  AssignmentRoute,
+  assignmentMetadata,
+  assignmentStaticParams,
+} from '@/components/assignment/assignment-route'
 
 export async function generateStaticParams() {
   return assignmentStaticParams('practice', true)
@@ -12,6 +16,8 @@ export async function generateMetadata({
   return assignmentMetadata(await params, 'practice')
 }
 
-export default async function Page({ params }: PageProps<'/[program]/[course]/[week]/practice-assignment/[term]'>) {
+export default async function Page({
+  params,
+}: PageProps<'/[program]/[course]/[week]/practice-assignment/[term]'>) {
   return <AssignmentRoute params={await params} kind="practice" />
 }
